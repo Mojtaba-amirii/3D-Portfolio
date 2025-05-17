df91d9b01b6ff9c97871b5396388b778ee701677
Mojtaba Amiri <mojtaba.a.amiri@outlook.com>
2025-05-17 12:46:22 +0200

    chore: update dependencies and refactor components
    - Updated @react-three/drei to version 10.0.7 and @react-three/fiber to version 9.1.2.
    - Upgraded React and ReactDOM to version 19.1.0.
    - Updated three.js to version 0.176.0.
    - Updated TypeScript types for React to version 19.1.4 and ReactDOM to version 19.1.5.
    - Changed @vitejs/plugin-react-swc to @vitejs/plugin-react in devDependencies.
    - Removed react-tilt from ServiceCard and Works components, replacing it with framer-motion for hover effects.
    - Cleaned up code in ServiceCard and Works components for better readability and performance.

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,18 +1,10 @@
-import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { fadeIn } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt
-      className=" xs:w-[250px] w-full "
-      options={{
-        max: 45,
-        scale: 1,
-        speed: 450,
-      }}
-    >
+    <motion.div className=" xs:w-[250px] w-full hover:rotate-x-51 hover:rotate-z-43 hover:transform-3d duration-500 transition-all will-change">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card "
@@ -24,7 +16,7 @@ const ServiceCard = ({ index, title, icon }) => {
           </h3>
         </div>
       </motion.div>
-    </Tilt>
+    </motion.div>
   );
 };
 
