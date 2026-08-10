43e05584712e8fe72f19b8bcbe80e89a4450988c
Mojtaba Amiri <mojtaba.a.amiri@outlook.com>
2026-08-10 19:56:30 +0200

    chore: update framer-motion and eslint versions; add build chunk size configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,37 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          minSize: 20000,
+          groups: [
+            {
+              name: "react-vendor",
+              test: /[\\/]node_modules[\\/](react|react-dom|react-router)/,
+              priority: 20,
+            },
+            {
+              name: "three-vendor",
+              test: /[\\/]node_modules[\\/](three|@react-three|maath)/,
+              priority: 15,
+            },
+            {
+              name: "vendor",
+              test: /[\\/]node_modules[\\/]/,
+              priority: 10,
+            },
+            {
+              name: "common",
+              minShareCount: 2,
+              minSize: 10000,
+              priority: 5,
+            },
+          ],
+        },
+      },
+    },
+  },
 });
